7f171d4fec2bfe57593c28397472ee6a627eeb14
Adam James <adam.vermeer2@gmail.com>
2021-09-03 07:24:05 -0700

    Cleaned up -main fn by pulling out save impl into its own function.

--- rss-saver.clj
+++ rss-saver.clj
@@ -68,14 +68,38 @@
                    (:updated content-map)
                    (:content content-map))}))
 
-(def cwd (.getCanonicalPath (io/file ".")))
-
 (def cli-options
   [["-h" "--help"]
    ["-u" "--url URL" "The URL of the RSS feed you want to save."]
    ["-d" "--dir DIR" "The directory where articles will be saved."
-    :default (str cwd "/posts")]
+    :default "./posts"]
    ["-c" "--clear" "Clear the cached copy of the previous feed."]])
+
+(defn clear!
+  [opts]
+  (let [prev-fname (str (:dir opts) "/" "previous-feed.atom")]
+    (sh "rm" "-f" prev-fname)))
+
+(defn save!
+  [opts]
+  (let [cur-str (slurp (:url opts))
+        prev-fname (str (:dir opts) "/" "previous-feed.atom")
+        prev-str (when (.exists (io/file prev-fname))
+                   (slurp prev-fname))
+        prev (when prev-str (feed-str->entries prev-str))
+        cur (feed-str->entries cur-str)
+        entries (remove (into #{} prev) cur)]
+    (if (> (count entries) 0)
+      (do
+        (println (str "Handling " (count entries) " entries."))
+        (sh "mkdir" "-p" (:dir opts))
+        (doseq [{:keys [id post]} (mapv entry->html entries)]
+          (let [fname (str
+                       (:dir opts) "/"
+                       (second (str/split id #"/")) ".html")]
+            (spit fname post)))
+        (spit prev-fname cur-str))
+      (println "No changes found in feed."))))
 
 (defn -main
   [& args]
@@ -89,21 +113,9 @@
       (println "Please specify feed URL.")
 
       :else
-      (let [cur-str (slurp (:url opts))
-            prev-str (when (.exists (io/file "previous-feed.atom"))
-                       (slurp "previous-feed.atom"))
-            prev (when prev-str (feed-str->entries prev-str))
-            cur (feed-str->entries cur-str)
-            entries (remove (into #{} prev) cur)]
-        (println (str "Handling " (count entries) " entries."))
-        (sh "mkdir" "-p" (:dir opts))
-        (into []
-              (for [{:keys [id post]} (mapv entry->html entries)]
-                (let [fname (str
-                             (:dir opts) "/"
-                             (second (str/split id #"/")) ".html")]
-                  (spit fname post))))
-        (spit "previous-feed.atom" cur-str)))))
+      (do
+        (when (:clear opts) (clear! opts))
+        (save! opts)))))
 
 (apply -main *command-line-args*)
 (shutdown-agents)
